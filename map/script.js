b1d9d26c600dd58d44a7e2bdd2139985c8f4582e
MMIODE12 <pieterprovoost@gmail.com>
2015-10-23 12:21:05 +0200

    degrees minutes seconds

--- map/script.js
+++ map/script.js
@@ -1,5 +1,13 @@
 var app = angular.module("app", ['leafcutter']);
 
+var trunc = function(x) {
+	if (x > 0) {
+		return Math.floor(x);
+	} else {
+		return Math.ceil(x);
+	}
+};
+
 app.controller("mapcontroller", function($scope, $filter, leafcuttermaps) {
 
 	$scope.log = "";
@@ -10,7 +18,31 @@ app.controller("mapcontroller", function($scope, $filter, leafcuttermaps) {
 		if (coords.length == 2) {
 			var lon = coords[0];
 			var lat = coords[1];
-			$scope.log = $scope.log + lon + " " + lat + "\n";
+
+			var dlon = trunc(lon);
+			var tlon = (lon - dlon) * 60;
+			var mlon = trunc(tlon);
+			var slon = Math.round((tlon - mlon) * 60);
+			
+			var dlat = trunc(lat);
+			var tlat = (lat - dlat) * 60;
+			var mlat = trunc(tlat);
+			var slat = Math.round((tlat - mlat) * 60);
+
+			var plon = "E";
+			if (dlon < 0) {
+				plon = "W";
+			}
+			var plat = "N";
+			if (dlat < 0) {
+				plat = "S";
+			}
+
+			var llon = plon + " " + Math.abs(dlon) + "°" + "" + Math.abs(mlon) + "'" + "" + Math.abs(slon) + "\"";
+			var llat = plat + " " + Math.abs(dlat) + "°" + "" + Math.abs(mlat) + "'" + "" + Math.abs(slat) + "\"";
+
+			$scope.log = $scope.log + lon + " " + lat + " (" + llon + " " + llat + ")\n";
+
 			var marker = L.marker([lat, lon]).addTo($scope.layer);
 		};
 	};
